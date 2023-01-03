545983a058861373a8222763f4a5e4ad7e483fd1
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-03 22:20:45 +0000

    Initial commit

---
+++ assignments.js
@@ -0,0 +1,52 @@
+/*const country = "Ukraine"
+const continent = "Eurasia"
+let population = 38000000
+
+console.log(country, continent, population)
+
+let isIsLand = true;
+const language = "Ukrainian"
+
+console.log(typeof isIsLand)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
+
+population = 39000000
+console.log(country, continent, population)
+
+
+
+//population = population / 2
+//console.log(population)
+//population++
+console.log(population)
+let populationFinland = 3000000
+console.log(population > populationFinland)
+console.log(population < 33000000)
+let description = country + " is in " + continent + " and its " + population + " speak " + language
+console.log(description)
+*/
+
+let massMark = 78
+let massJohn = 92
+
+let heightMark = 1.69
+let heightJohn = 1.95
+
+massMark = 95
+massJohn = 85
+
+heightMark = 1.88
+heightJohn = 1.76
+
+const bmiMark = massMark / heightMark ** 2
+const bmiJohn = massJohn / (heightJohn * heightJohn)
+const bmiCommon = bmiMark + bmiJohn
+console.log(bmiMark, bmiJohn, bmiCommon)
+
+let markHigherBMI = bmiMark > bmiJohn
+console.log(markHigherBMI)
+
+
+
---
+++ script.js
@@ -0,0 +1,92 @@
+/*let js = "amazing";
+console.log(40 + 8 * 3 - 1)
+console.log("Jonas")
+console.log(23)
+
+let firstName = "Holly";
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+
+console.log(typeof javascriptIsFun)
+//console.log(typeof 23)
+//console.log(typeof "Becca")
+
+javascriptIsFun = "YES!"
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991;
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
+
+// re-assigning the value, mutating the age variable
+let age = 30;
+age = 31
+
+// value cannot be changed
+
+const birthYear = 1991
+//birthYear = 1990
+
+var job = "pogrammer";
+job = "teacher"
+
+
+// math operators
+const now = 2037
+const ageBecca = now - 2020
+const ageJohn = now - 2019
+console.log(ageBecca, ageJohn)
+
+console.log(ageBecca * 2, ageJohn / 10, 2 ** 3)
+//2**3 means 2 to the power of 3=2*2*2
+
+const firstName = "John"
+const lastName = "Hugh"
+console.log(firstName + " " + lastName)
+
+let x = 10 + 5 //15
+x += 10; // x = x+10=25
+
+x *= 4 // x=x*4=100
+x++ // x= x+1
+x-- //x = x-1
+console.log(x)
+
+// comparison operators
+
+console.log(ageBecca > ageJohn) // <,>,<=,>=
+console.log(ageBecca >= 18)
+
+const isFullAge = ageBecca >= 18
+
+console.log(now - 1991 > now - 2018)
+
+
+const now = 2037
+const ageBecca = now - 2020
+const ageJohn = now - 2019
+console.log(now - 1991 > now - 2018)
+
+
+let x, y;
+x = y = 25 - 10 - 5//x=y=10, x=10
+console.log(x, y)
+
+
+const averageAge = (ageBecca + ageJohn) / 2
+console.log(ageBecca)
+console.log(ageJohn)
+console.log(averageAge)
+*/
